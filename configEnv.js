8895dbc49764c15bd17e1fc8eff228c0b02a11e2
George Apostolov <george@livedemo.ai>
2026-03-24 00:33:15 -0400

    Add github actions and additional docker config for running locally

--- configEnv.js
+++ configEnv.js
@@ -1,15 +1,17 @@
+import LOCAL_CONFIG from './configs/prod.js'
 import DEV_CONFIG from './configs/dev.js'
 import STAGING_CONFIG from './configs/staging.js'
 import PROD_CONFIG from './configs/prod.js'
 import fs from 'fs'
 
 const CONFIGS = {
+  local: LOCAL_CONFIG,
   dev: DEV_CONFIG,
   staging: STAGING_CONFIG,
   prod: PROD_CONFIG
 }
 // let ENV = 'prod'
-let ENV = process.env.ENV || 'dev'
+let ENV = process.env.ENV || 'local'
 console.log("Environment built for " + ENV)
 
 fs.writeFileSync('./src/config.json', JSON.stringify(CONFIGS[ENV]))
